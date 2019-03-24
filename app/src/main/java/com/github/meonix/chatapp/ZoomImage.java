111f5eeccc0be9a1c26ae90baa4ccd8180776906
Meonix <37493834+Meonix@users.noreply.github.com>
2019-03-24 19:46:32 +0700

    adding download Image from ZoomImage activity

--- app/src/main/java/com/github/meonix/chatapp/ZoomImage.java
+++ app/src/main/java/com/github/meonix/chatapp/ZoomImage.java
@@ -1,8 +1,10 @@
 package com.github.meonix.chatapp;
 
-import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,17 +12,19 @@ import android.widget.Toast;
 
 import com.github.chrisbanes.photoview.PhotoView;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class ZoomImage extends AppCompatActivity {
     private String linkImage;
     private PhotoView ZoomImageView;
-    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoom_image);
-        progressDialog = new ProgressDialog(this);
         ZoomImageView = findViewById(R.id.pvZoomImage);
         linkImage = getIntent().getExtras().get("imageURL").toString();
         Picasso.get().load(linkImage).into(ZoomImageView);
@@ -47,13 +51,42 @@ public class ZoomImage extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.downloadImage:
-                progressDialog.setTitle("Image is Downloading...");
-                progressDialog.show();
+                Picasso.get()
+                        .load(linkImage)
+                        .into(new Target() {
+                                  @Override
+                                  public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                                      try {
+                                          Toast.makeText(ZoomImage.this, "Image has been Downloaded....", Toast.LENGTH_SHORT).show();
+                                          // Get the directory for the user's public pictures directory.
+                                          File file = new File(Environment.getExternalStoragePublicDirectory(
+                                                  Environment.DIRECTORY_PICTURES), "Miretty");
 
+                                          final File externalFile = new File(file, System.currentTimeMillis() + ".jpg");
 
+                                          FileOutputStream out = new FileOutputStream(externalFile);
+                                          bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
 
-                progressDialog.dismiss();
-                Toast.makeText(this, "Image is Downloading...", Toast.LENGTH_SHORT).show();
+                                          out.flush();
+                                          out.close();
+                                      } catch (Exception e) {
+                                          // some action
+                                          Toast.makeText(ZoomImage.this, "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                      }
+                                  }
+
+                                  @Override
+                                  public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                                      Toast.makeText(ZoomImage.this, "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                  }
+
+                                  @Override
+                                  public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                                  }
+                              }
+                        );
                 return true;
             default:
                 return super.onContextItemSelected(item);
